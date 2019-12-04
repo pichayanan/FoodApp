ee98bd882190b60873f0c56e68bdb86265fcdffe
pichayanan <38246387+pichayanan@users.noreply.github.com>
2019-12-04 23:36:37 +0700

    category

--- app/src/main/java/com/pichayanan/appfood/List_resturant.java
+++ app/src/main/java/com/pichayanan/appfood/List_resturant.java
@@ -2,7 +2,9 @@ package com.pichayanan.appfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class List_resturant extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class List_resturant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_resturant2);
+    }
+    public void menulist(View view) {
+
+        final String TAG = "menulist";
+
+        Intent ResList = new Intent(List_resturant.this, menu_list.class);
+        startActivity(ResList);
+
+
+    }
+    public void backbtn(View view) {
+
+        final String TAG = "backbtn";
+
+        Intent ResList = new Intent(List_resturant.this, List_Category.class);
+        startActivity(ResList);
+
+
     }
 }
